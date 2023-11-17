643846ac582f504a3135ad3e5266fbf7f8940d80
John-thabuks <thabuksjohn@gmail.com>
2023-11-17 13:53:05 +0300

    Organized my components into a components folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Header from "./components/Header"
+
 
 function App(){
+ 
   return (
-    <div className='App'>
+    <div className='container'>
 
-      <h1>Hello</h1>
-
+      <Header/>
     </div>
   )
 }
