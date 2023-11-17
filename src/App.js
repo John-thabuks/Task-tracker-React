7bd32e42a2e3d6e016ec864e30d4cf52dbb61c5a
John-thabuks <thabuksjohn@gmail.com>
2023-11-17 14:56:39 +0300

    Created components and learned more regarding PropTypes and defaultProps

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App(){
   return (
     <div className='container'>
 
-      <Header/>
+      <Header title="Task Tracker"/>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import PropTypes from "prop-types"
+import Button from "./Button"
 
-const Header = () => {
+const Header = ({title}) => {
+    const onClick = ()=>{
+        console.log("click")
+    }
+
   return (
-    <header>
+    <header className='header'>
 
-        <h1>Task Tracker</h1>
+        <h1 >{title }</h1>
+        <Button color= "green" text= "Add" onClick = {onClick}/>
     </header>
   )
 }
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+Header.defaultProps ={
+    title: "Task Tracker"
+}
+
+
+// Css in JS
+// const headingStyle = {
+//     color: "red",
+//     backgroundColor: "black"
+// }
 
 export default Header
